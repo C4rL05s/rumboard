238945ab13fb5a60a38bbd744a765b4f4d09c913
C_EscS <carlos.escobar4@upr.edu>
2026-05-12 14:27:33 -0400

    Changes to socket.js to handle user connection and events for life notifications

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,12 @@ import { io } from 'socket.io-client'
 
 const socket = io('http://localhost:3000')
 
+// Get username from local storage
+const username = localStorage.getItem('username')
+if (username) {
+    // Emit event to put user in a room
+    socket.emit('connect-user', username)
+}
+// Else user will not be put in a room
+
 export default socket
